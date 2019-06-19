31a678533883b7ebca34683efae84464f7e4bf9a
root <root@ip-172-31-22-5.ap-southeast-1.compute.internal>
2019-06-19 05:45:33 +0000

    commit keeper_api

---
+++ index.js
@@ -0,0 +1,33 @@
+var port  = process.env.port
+var es_host = process.env.es_host
+var es_port = process.env.es_port
+var es_index = process.env.es_index
+var es_type = process.env.es_type
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+var dateFormat = require('dateformat');
+var rp = require('request-promise');
+app.use(bodyParser.json());
+app.use(require('express-promise')());
+
+app.post('/keep_this_data', function (req, res) {
+  console.log(req.body)
+  postToEs(req.body)
+  res.send("done")
+})
+
+function postToEs(json) {
+  url = es_host + ":" + es_port + "/" + es_index + "/" + es_type
+  return rp({
+        uri: url,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: json,
+        json: true
+    })
+}
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
